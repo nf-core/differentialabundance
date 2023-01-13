21340293452c8a92d2bf8a2515103d6a9b0cbbd4
Jonathan Manning <jonathan.manning@healx.io>
2023-01-13 16:49:08 +0000

    Update pipeline groovy

--- lib/WorkflowDifferentialabundance.groovy
+++ lib/WorkflowDifferentialabundance.groovy
@@ -12,11 +12,10 @@ class WorkflowDifferentialabundance {
     public static void initialise(params, log) {
         genomeExistsError(params, log)
 
-
-//        if (!params.fasta) {
-//            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-//            System.exit(1)
-//        }
+        if (!params.gtf) {
+            log.error "Genome GTF file not specified with e.g. '--gtf genome.gtf' or via a detectable config file."
+            System.exit(1)
+        }
     }
 
     //
