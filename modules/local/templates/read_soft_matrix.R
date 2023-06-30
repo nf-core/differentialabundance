718c0b31ec14cb6f78477d2f8643c3d6020bfd31
Azedine Zoufir <azedine.zoufir@healx.io>
2023-06-30 11:18:57 +0100

    fix linting/indents in read_soft_matrix.R

--- modules/local/templates/read_soft_matrix.R
+++ modules/local/templates/read_soft_matrix.R
@@ -8,14 +8,14 @@
 # @param x Long-form argument list like --opt1 val1 --opt2 val2
 # return named list of options and values similar to optparse
 parse_args <- function(x){
-  args_list <- unlist(strsplit(x, ' ?--')[[1]])[-1]
-  args_vals <- lapply(args_list, function(x) scan(text=x, what='character', quiet = TRUE))
+    args_list <- unlist(strsplit(x, ' ?--')[[1]])[-1]
+    args_vals <- lapply(args_list, function(x) scan(text=x, what='character', quiet = TRUE))
 
-  # Ensure the option vectors are length 2 (key/ value) to catch empty ones
-  args_vals <- lapply(args_vals, function(z){ length(z) <- 2; z})
+    # Ensure the option vectors are length 2 (key/ value) to catch empty ones
+    args_vals <- lapply(args_vals, function(z){ length(z) <- 2; z})
 
-  parsed_args <- structure(lapply(args_vals, function(x) x[2]), names = lapply(args_vals, function(x) x[1]))
-  parsed_args[! is.na(parsed_args)]
+    parsed_args <- structure(lapply(args_vals, function(x) x[2]), names = lapply(args_vals, function(x) x[1]))
+    parsed_args[! is.na(parsed_args)]
 }
 # From affy/justRMA (pinin4fjords)
 # Round numeric dataframe columns to fixed decimal places by applying
@@ -25,19 +25,19 @@ parse_args <- function(x){
 # @param digits How many decimal places to round to?
 # @return output Data frame
 round_dataframe_columns <- function(df, columns = NULL, digits = 8){
-  if (is.null(columns)){
-    columns <- colnames(df)
-  }
+    if (is.null(columns)){
+        columns <- colnames(df)
+    }
 
-  df[,columns] <- format(data.frame(df[, columns]), nsmall = digits)
+    df[,columns] <- format(data.frame(df[, columns]), nsmall = digits)
 
-  # Convert columns back to numeric
+    # Convert columns back to numeric
 
-  for (c in columns) {
-    df[[c]][grep("^ *NA\$", df[[c]])] <- NA
-    df[[c]] <- as.numeric(df[[c]])
-  }
-  df
+    for (c in columns) {
+        df[[c]][grep("^ *NA\$", df[[c]])] <- NA
+        df[[c]] <- as.numeric(df[[c]])
+    }
+    df
 }
 
 ############################
@@ -45,16 +45,16 @@ round_dataframe_columns <- function(df, columns = NULL, digits = 8){
 ############################
 
 opt <- list(
-  querygse = '',
-  metacols = ''
+    querygse = '',
+    metacols = ''
 )
 args_opt <- parse_args('$task.ext.args')
 for ( ao in names(args_opt)){
-  if (! ao %in% names(opt)){
-    stop(paste("Invalid option:", ao))
-  }else{
-    opt[[ao]] <- args_opt[[ao]]
-  }
+    if (! ao %in% names(opt)){
+        stop(paste("Invalid option:", ao))
+    }else{
+        opt[[ao]] <- args_opt[[ao]]
+    }
 }
 
 ############################
@@ -78,8 +78,8 @@ write.table(fData(eset)[,feature_cols],
 
 # if data is not log scale, transform it as needed for limma downstream
 if(max(exprs(eset),na.rm=T) > 20) { # a bit dirty, needs proper solution later...
-  exprs(eset)[exprs(eset) <= 0 | is.na(exprs(eset))] <- .001
-  exprs(eset) <- log2(exprs(eset))
+    exprs(eset)[exprs(eset) <= 0 | is.na(exprs(eset))] <- .001
+    exprs(eset) <- log2(exprs(eset))
 }
 
 output_prefix <- '$task.ext.prefix'
@@ -87,14 +87,14 @@ saveRDS(eset, file = paste0(output_prefix, 'eset.rds'))
 
 # write intensity matrix (normalised)
 write.table(
-  data.frame(
-    probe_id = rownames(eset),
-    round_dataframe_columns(as.data.frame(exprs(eset))),
-    check.names = FALSE
-  ),
-  file = paste0(output_prefix, 'matrix.tsv'),
-  col.names = TRUE, row.names = FALSE,
-  sep = '\t', quote = FALSE
+    data.frame(
+        probe_id = rownames(eset),
+        round_dataframe_columns(as.data.frame(exprs(eset))),
+        check.names = FALSE
+    ),
+    file = paste0(output_prefix, 'matrix.tsv'),
+    col.names = TRUE, row.names = FALSE,
+    sep = '\t', quote = FALSE
 )
 
 
@@ -111,9 +111,9 @@ r.version <- strsplit(version[['version.string']], ' ')[[1]][3]
 geoquery.version <- as.character(packageVersion("GEOquery"))
 
 writeLines(
-  c(
-    '"${task.process}":',
-    paste('    r-base:', r.version),
-    paste('    bioconductor-:', geoquery.version)
-  ),
-  'versions.yml')
+    c(
+        '"${task.process}":',
+        paste('    r-base:', r.version),
+        paste('    bioconductor-:', geoquery.version)
+    ),
+    'versions.yml')
